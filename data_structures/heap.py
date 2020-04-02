d50005cac5923d98d32d596665ec91610d4720e7
akshay <akshayk.vnit@gmail.com>
2020-04-02 21:27:32 +0530

    added comments for heaps & heap sort

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -1,21 +1,41 @@
 def max_heapify(arr, i):
+    # left child of node at index i
     left = 2 * i
+    # right child of node at index i
     right = 2 * i + 1
+    # if left child index is within the array
+    # and left child is larger than parent, then
+    # update largest variable to left child index
+    # else let it be the parent index
     if left < len(arr) and arr[left] > arr[i]:
         largest = left
     else:
         largest = i
 
+    # if right child index is within the array
+    # and right child is larger than largest element, 
+    # then update largest variable to right child index
+    # else let it be the previously set value
     if right < len(arr) and arr[right] > arr[largest]:
         largest = right
 
+    # if parent is not the largest value, then
+    # swap the largest element with the parent
+    # to satisfy the max heap property
     if not largest == i:
         arr[largest], arr[i] = arr[i], arr[largest]
+        # recursively call max_heapify on the swapped
+        # element. Since it was modified (swapped),
+        # there is a chance that it may have violated
+        # the max heap property w.r.t. other nodes
         arr = max_heapify(arr, largest)
     
     return arr
 
 def build_max_heap(arr):
+    # in a heap, the elements from n // 2 to the end
+    # are leaf nodes. So, apply max_heapify on the 
+    # non-leaf nodes i.e. from 0 to n // 2.
     for i in range(len(arr) // 2, -1, -1):
         arr = max_heapify(arr, i)
     return arr
--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -3,9 +3,17 @@ sys.path.insert(1, '../data_structures/')
 import heap
 
 def heap_sort(arr):
+    # build max heap from given array
     arr = heap.build_max_heap(arr)
     for i in range(len(arr) - 1, 0, -1):
+        # swap the root element (which will be the largest),
+        # with the last element of a shortened array (array
+        # shortens at every step of this loop). Consequently,
+        # the largest elements go to the end in each step
         arr[0], arr[i] = arr[i], arr[0]
+        # since we swapped elements, max heap property may
+        # have been violated, so build max heap again with
+        # shortened array
         arr[ : i] = heap.build_max_heap(arr[ : i])        
     return arr
 
